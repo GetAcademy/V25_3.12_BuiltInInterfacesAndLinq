70da80898ab4a7c8bf8891b3e753e2708e133e47
dagbru <dag.sorensen@gmail.com>
2025-06-11 08:45:03 +0200

    initial commit

---
+++ V25_3.12_BuiltInInterfacesAndLinq/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = { 1, 2, 3 };
+foreach (var n in numbers)
+{
+    Console.WriteLine(n);
+}
+
+List<string> names = new() { "Ada", "Bob", "Charlie" };
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
