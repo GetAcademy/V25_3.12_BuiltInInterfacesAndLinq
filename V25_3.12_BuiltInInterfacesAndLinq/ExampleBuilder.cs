2be08c0f26f104ae1c74204613a205912a87e941
dagbru <dag.sorensen@gmail.com>
2025-06-19 13:05:03 +0200

    eeeyoo

---
+++ V25_3.12_BuiltInInterfacesAndLinq/ExampleBuilder.cs
@@ -0,0 +1,76 @@
+﻿using V25_3._12_BuiltInInterfacesAndLinq.CustomEnumerable;
+using V25_3._12_BuiltInInterfacesAndLinq.Extension;
+using V25_3._12_BuiltInInterfacesAndLinq.Foreach;
+using V25_3._12_BuiltInInterfacesAndLinq.LINQ;
+
+namespace V25_3._12_BuiltInInterfacesAndLinq;
+
+public class ExampleBuilder
+{
+    private readonly Dictionary<ExampleName, IExample> _examples = new();
+    public ExampleBuilder Add<T>() where T : IExample, new()
+    {
+        var example = new T();
+        _examples.TryAdd(example.ExampleName, example);
+
+        return this;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    public ExampleBuilder AddExample(ExampleName exampleName)
+    {
+        IExample? example = null;
+        switch (exampleName)
+        {
+            case ExampleName.Foreach:
+                example = new ForeachExample();
+                break;
+            case ExampleName.CustomEnumerable:
+                example = new CustomEnumerableExample();
+                break;
+            case ExampleName.Extension:
+                example = new ExtensionExample();
+                break;
+            case ExampleName.Linq:
+                example = new LinqExample();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(exampleName), exampleName, null);
+        }
+
+        _examples.Add(exampleName, example);
+
+        return this;
+    }
+
+
+    public ExampleBuilder AddLinqExample()
+    {
+        _examples.Add(ExampleName.Linq, new LinqExample());
+
+        return this;
+    }
+
+
+    public ExampleBuilder AddForeachExample()
+    {
+        _examples.Add(ExampleName.Foreach, new ForeachExample());
+
+        return this;
+    }
+
+    public Dictionary<ExampleName, IExample> Build()
+    {
+        return _examples;
+    }
+}
--- V25_3.12_BuiltInInterfacesAndLinq/Program.cs
+++ V25_3.12_BuiltInInterfacesAndLinq/Program.cs
@@ -1,15 +1,38 @@
-﻿
-using V25_3._12_BuiltInInterfacesAndLinq;
+﻿using V25_3._12_BuiltInInterfacesAndLinq;
 using V25_3._12_BuiltInInterfacesAndLinq.CustomEnumerable;
 using V25_3._12_BuiltInInterfacesAndLinq.Extension;
 using V25_3._12_BuiltInInterfacesAndLinq.Foreach;
 using V25_3._12_BuiltInInterfacesAndLinq.LINQ;
 
-IExample exampleToRun = null;
+// var examples = 
+//     new ExampleBuilder()
+//         .AddExample(ExampleName.Linq)
+//         .AddExample(ExampleName.Extension)
+//         .AddExample(ExampleName.CustomEnumerable)
+//         .Build();
+
+var exampleToRun = ExampleName.Linq;
+var examples = new ExampleBuilder()
+    .Add<ForeachExample>()
+    .Add<LinqExample>()
+    .Add<CustomEnumerableExample>()
+    .Add<ExtensionExample>()
+    .Build();
+
+
+examples[exampleToRun].Run();
+foreach (var example in examples)
+{
+    example.Value.Run();
+}
+
+
+
+// IExample exampleToRun;
 
 // exampleToRun = new ForeachExample();
 // exampleToRun = new CustomEnumerableExample();
 // exampleToRun = new ExtensionExample();
-exampleToRun = new LinqExample();
-
-exampleToRun.Run();
+// exampleToRun = new LinqExample();
+//
+// exampleToRun.Run();
